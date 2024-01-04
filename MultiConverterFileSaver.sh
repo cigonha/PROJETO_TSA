1c7321daa22b0285f57bd82ee8a327fe05c25c07
Diogo Pedro <149061816+cigonha@users.noreply.github.com>
2024-01-04 09:21:40 +0000

    Sript Final (dia 4 janeiro)

--- MultiConverterFileSaver.sh
+++ MultiConverterFileSaver.sh
@@ -126,7 +126,7 @@ HorasDia=$(date +%T)
     }
 
 
-#Função para Converter Unidades de Tempo
+#Funções para Converter Unidades de Tempo
 
     ## Função para converter horas para minutos
     horas_minutos() {
@@ -174,7 +174,115 @@ HorasDia=$(date +%T)
     }  
 
 
-#Função para Converter Moedas
+#Funções para Converter Moedas
+
+    ## Função para converter euros para reais
+    euros_reais(){
+        local euros="$1"
+        local reais=$(echo "scale=3; $euros * 5.35" | bc)
+        echo "Resultado da Conversão: $euros € é igual a $reais R$."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $euros € é igual a $reais R$." >> $historico
+        echo
+    }
+
+    ## Função para converter euros para dólares
+    euros_dolares(){
+        local euros="$1"
+        local dolares=$(echo "scale=3; $euros * 1.078" | bc)
+        echo "Resultado da Conversão: $euros € é igual a $dolares $."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $euros € é igual a $dolares $." >> $historico
+        echo
+    }   
+
+    ## Função para converter euros para ienes
+    euros_ienes(){
+        local euros="$1"
+        local ienes=$(echo "scale=3; $euros * 157.26" | bc)
+        echo "Resultado da Conversão: $euros € é igual a $ienes ¥."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $euros € é igual a $ienes ¥." >> $historico
+        echo
+    }   
+
+    ## Função para converter reais para euros
+    reais_euros(){
+        local reais="$1"
+        local euros=$(echo "scale=3; $reais / 5.35" | bc)
+        echo "Resultado da Conversão: $reais R$ é igual a $euros €."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $reais R$ é igual a $euros €." >> $historico
+        echo
+    }  
+    
+    ## Função para converter reais para dólares
+    reais_dolares(){
+        local reais="$1"
+        local dolares=$(echo "scale=3; $reais / 4.97" | bc)
+        echo "Resultado da Conversão: $reais R$ é igual a $dolares $."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $reais R$ é igual a $dolares $." >> $historico
+        echo
+    }  
+
+    ## Função para converter reais para ienes
+    reais_ienes(){
+        local reais="$1"
+        local ienes=$(echo "scale=3; $reais / 0.034" | bc)
+        echo "Resultado da Conversão: $reais R$ é igual a $ienes ¥."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $reais R$ é igual a $ienes ¥." >> $historico
+        echo
+    }  
+
+    ## Função para converter dólares para euros
+    dolares_euros(){
+        local dolares="$1"
+        local euros=$(echo "scale=3; $dolares / 1.078" | bc)
+        echo "Resultado da Conversão: $dolares $ é igual a $euros €."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $dolares $ é igual a $euros €." >> $historico
+        echo
+    } 
+    
+    ## Função para converter dólares para reais
+    dolares_reais(){
+        local dolares="$1"
+        local reais=$(echo "scale=3; $dolares * 4.97" | bc)
+        echo "Resultado da Conversão: $dolares $ é igual a $reais R$."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $dolares $ é igual a $reais R$." >> $historico
+        echo
+    }
+
+    ## Função para converter dólares para ienes
+    dolares_ienes(){
+        local dolares="$1"
+        local ienes=$(echo "scale=3; $dolares * 145.80" | bc)
+        echo "Resultado da Conversão: $dolares $ é igual a $ienes ¥."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $dolares $ é igual a $ienes ¥." >> $historico
+        echo
+    }
+
+    ## Função para converter ienes para euros
+    ienes_euros(){
+        local ienes="$1"
+        local euros=$(echo "scale=3; $ienes / 157.26" | bc)
+        echo "Resultado da Conversão: $ienes ¥ é igual a $euros €."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $ienes ¥ é igual a $euros €." >> $historico
+        echo
+    }
+
+    ## Função para converter ienes para dólares
+    ienes_dolares(){
+        local ienes="$1"
+        local dolares=$(echo "scale=3; $ienes / 145.80" | bc)
+        echo "Resultado da Conversão: $ienes ¥ é igual a $dolares $."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $ienes ¥ é igual a $dolares $." >> $historico
+        echo
+    }
+
+    ## Função para converter ienes para reais
+    ienes_reais(){
+        local ienes="$1"
+        local reais=$(echo "scale=3; $ienes * 0.034" | bc)
+        echo "Resultado da Conversão: $ienes ¥ é igual a $reais R$."
+        echo "$DiaSemana, $DiaAno, $HorasDia - $ienes ¥ é igual a $reais R$." >> $historico
+        echo
+    }
 
 
 # Script Principal
@@ -235,44 +343,68 @@ while true; do
             case $option in
                 1)  
                     echo
+                    echo "#### A CONVERTER ####"
+                    echo
                     read -p "Coloca um tamanho em Metros: " distance
                     result=$(metros_quilometros "$distance")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 2)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em Quilómetros: " distance
                     result=$(quilometros_metros "$distance")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 3)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em Metros: " distance
                     result=$(metros_centimetros "$distance")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 4)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em Centímetros: " distance
                     result=$(centimetros_metros "$distance")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 5)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em Quilómetros: " distance
                     result=$(quilometros_centimetros "$distance")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 6)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em Centímetros: " distance
                     result=$(centimetros_quilometros "$distance")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
                     echo
                     ;;
                 7)  
@@ -309,44 +441,68 @@ while true; do
             case $option in
                 1)  
                     echo
+                    echo "#### A CONVERTER ####"
+                    echo
                     read -p "Coloca um tamanho em bytes: " size
                     result=$(bytes_kilobytes "$size")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 2)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em bytes: " size
                     result=$(bytes_megabytes "$size")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 3)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em bytes:" size
                     result=$(bytes_gigabytes "$size")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 4)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em bytes: " size
                     result=$(bytes_terabytes "$size")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 5)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em megabytes: " size
                     result=$(megabytes_bytes "$size")
                     echo "$result"
                     echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 6)  
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tamanho em gigabytes: " size
                     result=$(gigabytes_megabytes "$size")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
                     echo
                     ;;
                 7) 
@@ -382,33 +538,58 @@ while true; do
             case $option in
                 1)
                     echo
+                    echo "#### A CONVERTER ####"
+                    echo
                     read -p "Coloca um tempo em horas: " time
                     result=$(horas_minutos "$time")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 2)
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tempo em horas: " time
                     result=$(horas_segundos "$time")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 3)
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tempo em minutos: " time
                     result=$(minutos_horas "$time")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 4)
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tempo em minutos: " time
                     result=$(minutos_segundos "$time")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 5)
+                    echo
+                    echo "#### A CONVERTER ####"
                     echo
                     read -p "Coloca um tempo em segundos: " time
                     result=$(segundos_minutos "$time")
                     echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo
                     ;;
                 6) 
                     break 
@@ -427,10 +608,177 @@ while true; do
         done
         ;;
 
-        4) 
+        4) while true; do 
+            echo
+            echo "**** Conversor de Moedas ****"
+            echo "1. Euros para Reais" 
+            echo "2. Euros para Dólares" 
+            echo "3. Euros para Ienes" 
+            echo "4. Reais para Euros" 
+            echo "5. Reais para Dólares" 
+            echo "6. Reais para Ienes"
+            echo "7. Dólares para Euros"
+            echo "8. Dólares para Reais"
+            echo "9. Dólares para Ienes"
+            echo "10. Ienes para Euros"
+            echo "11. Ienes para Dólares"
+            echo "12. Ienes para Reais" 
+            echo "13. Voltar ao Menu Principal"
+            echo "14. Sair do Conversor (Exit)"
+            echo
+            read -p "Escolha uma opção (1-14): " option
+
+            case $option in
+                1)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em euros: " value 
+                    result=$(euros_reais "$value")
+                    echo "$result"
+                    echo   
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo 
+                    ;;
+                2)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em euros: " value 
+                    result=$(euros_dolares "$value")
+                    echo "$result"
+                    echo  
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo   
+                    ;;
+                3)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em euros: " value 
+                    result=$(euros_ienes "$value")
+                    echo "$result"
+                    echo 
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo   
+                    ;;
+                4)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em reais: " value 
+                    result=$(reais_euros "$value")
+                    echo "$result"
+                    echo  
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo    
+                    ;;
+                5)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em reais: " value 
+                    result=$(reais_dolares "$value")
+                    echo "$result"
+                    echo   
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo 
+                    ;;
+                6)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em reais: " value 
+                    result=$(reais_ienes "$value")
+                    echo "$result"
+                    echo 
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo    
+                    ;;
+                7)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em dólares: " value 
+                    result=$(dolares_euros "$value")
+                    echo "$result"
+                    echo 
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo    
+                    ;;
+                8)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em dólares: " value 
+                    result=$(dolares_reais "$value")
+                    echo "$result"
+                    echo
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo     
+                    ;;
+                9)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em dólares: " value 
+                    result=$(dolares_ienes "$value")
+                    echo "$result"
+                    echo 
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo      
+                    ;;
+                10)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em ienes: " value 
+                    result=$(ienes_euros "$value")
+                    echo "$result"
+                    echo  
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo    
+                    ;;
+                11)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em ienes: " value 
+                    result=$(ienes_dolares "$value")
+                    echo "$result"
+                    echo   
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo   
+                    ;;
+                12)
+                    echo
+                    echo "#### A CONVERTER ####"
+                    echo
+                    read -p "Coloca um valor em ienes: " value 
+                    result=$(ienes_reais "$value")
+                    echo "$result"
+                    echo  
+                    echo "#### CONVERSÃO REALIZADA ####"
+                    echo   
+                    ;;
+                13) 
+                    break 
+                    ;;
+                14)  
+                    echo
+                    echo "#### A sair do MultiConverter ####" 
+                    exit 0
+                    ;;  
+                *)
+                    echo
+                    echo "#### Opção Inválida. Por Favor escolha entre 1 a 14 ####"
+                    echo
+                    ;;
+            esac
+        done
         ;;
         
-        5)  while true; do 
+        5) while true; do 
             echo
             echo "**** Histórico MultiConverter ****"
             echo "1. Ver o histórico"
@@ -450,18 +798,19 @@ while true; do
                 2)  
                     echo
                     echo "#### Estamos a fazer um backup do histórico e posteriormente a limpá-lo ####"
-                            echo
+                    echo
                     # Verificar se o diretório de backups histórico não existe
                         if [ ! -d "$backups_historico" ]; then
                     # Se não existir, cria o diretório
                             mkdir -p "$backups_historico"
                             echo "O diretório \"$backups_historico\" foi criado com sucesso."
                         fi
-                    # Se já exitir, copia o arquivo histórico para o diretório de backups e posteriormente limpa todo o histórico
+                    # Se já exitir, copia o ficheiro histórico para o diretório de backups
                             cp historico.txt "$backups_historico/$nome_backups"
                             echo "O ficheiro historico.txt foi copiado com sucesso." 
-                            echo
+                    echo
                     echo "#### Operações realizadas com sucesso ####"
+                    # Posteriormente vai limpar todo o histórico
                             rm historico.txt
                             echo "" > historico.txt
                     echo
